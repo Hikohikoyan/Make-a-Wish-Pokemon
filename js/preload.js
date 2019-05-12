615bab4e0d85560a53c75000a5b6b745b27d0c71
Becky Lu <243594043@qq.com>
2019-05-12 22:23:23 +0800

    img url is correct now

--- js/preload.js
+++ js/preload.js
@@ -14,7 +14,7 @@ $(function(){
     var imgdownload = new createjs.LoadQueue(true);
     function handleComplete(){
         var src=$("#rotate").attr("src");
-        src=src.replace("http://182.254.161.178/test/laravel/public/img/rotate/","");
+        src=src.replace("http://182.254.161.178/laravel/public/img/rotate/","");
         src=src.replace(".png","");
         console.log("completed");
         console.log(src);
@@ -25,52 +25,52 @@ $(function(){
     // console.log(imgdownload);
     // console.log(imgdownload.loadManifest);
     imgdownload.loadManifest([
-        "http://182.254.161.178/test/laravel/public/img/smallback.jpg",
-        "http://182.254.161.178/test/laravel/public/img/background.png",
-        "http://182.254.161.178/test/laravel/public/img/elfborder.png",//index.html
-        "http://182.254.161.178/test/laravel/public/img/ballborder.png",
-        "http://182.254.161.178/test/laravel/public/img/title.png",
-        "http://182.254.161.178/test/laravel/public/img/wish.png",
-        "http://182.254.161.178/test/laravel/public/img/help.png",
-        "http://182.254.161.178/test/laravel/public/img/mine.png",
-        "http://182.254.161.178/test/laravel/public/img/rule.png",
-        "http://182.254.161.178/test/laravel/public/img/49.png",
-        "http://182.254.161.178/test/laravel/public/img/41.png",
-        "http://182.254.161.178/test/laravel/public/img/21.png",
-        "http://182.254.161.178/test/laravel/public/img/bigelfboder.png",
-        "http://182.254.161.178/test/laravel/public/img/middle.png",//wsih.html
-        "http://182.254.161.178/test/laravel/public/img/next.png",
-        "http://182.254.161.178/test/laravel/public/img/again.png",
-        "http://182.254.161.178/test/laravel/public/img/gohelp.png",
-        "http://182.254.161.178/test/laravel/public/img/change.png",
-        "http://182.254.161.178/test/laravel/public/img/wagada.png",
-        "http://182.254.161.178/test/laravel/public/img/cancel.png",//new
-        "http://182.254.161.178/test/laravel/public/img/back.png",
-        "http://182.254.161.178/test/laravel/public/img/return.png",
-        "http://182.254.161.178/test/laravel/public/img/welcome.png",
+        "http://182.254.161.178/laravel/public/img/smallback.jpg",
+        "http://182.254.161.178/laravel/public/img/background.png",
+        "http://182.254.161.178/laravel/public/img/elfborder.png",//index.html
+        "http://182.254.161.178/laravel/public/img/ballborder.png",
+        "http://182.254.161.178/laravel/public/img/title.png",
+        "http://182.254.161.178/laravel/public/img/wish.png",
+        "http://182.254.161.178/laravel/public/img/help.png",
+        "http://182.254.161.178/laravel/public/img/mine.png",
+        "http://182.254.161.178/laravel/public/img/rule.png",
+        "http://182.254.161.178/laravel/public/img/49.png",
+        "http://182.254.161.178/laravel/public/img/41.png",
+        "http://182.254.161.178/laravel/public/img/21.png",
+        "http://182.254.161.178/laravel/public/img/bigelfboder.png",
+        "http://182.254.161.178/laravel/public/img/middle.png",//wsih.html
+        "http://182.254.161.178/laravel/public/img/next.png",
+        "http://182.254.161.178/laravel/public/img/again.png",
+        "http://182.254.161.178/laravel/public/img/gohelp.png",
+        "http://182.254.161.178/laravel/public/img/change.png",
+        "http://182.254.161.178/laravel/public/img/wagada.png",
+        "http://182.254.161.178/laravel/public/img/cancel.png",//new
+        "http://182.254.161.178/laravel/public/img/back.png",
+        "http://182.254.161.178/laravel/public/img/return.png",
+        "http://182.254.161.178/laravel/public/img/welcome.png",
         // "img/423.png",
-        "http://182.254.161.178/test/laravel/public/img/21.png",//rotate
-        "http://182.254.161.178/test/laravel/public/img/rotate/1.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/2.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/3.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/4.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/5.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/6.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/7.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/8.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/9.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/10.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/11.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/12.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/13.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/14.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/15.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/16.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/17.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/18.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/19.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/20.png",
-        "http://182.254.161.178/test/laravel/public/img/rotate/21.png",
+        "http://182.254.161.178/laravel/public/img/21.png",//rotate
+        "http://182.254.161.178/laravel/public/img/rotate/1.png",
+        "http://182.254.161.178/laravel/public/img/rotate/2.png",
+        "http://182.254.161.178/laravel/public/img/rotate/3.png",
+        "http://182.254.161.178/laravel/public/img/rotate/4.png",
+        "http://182.254.161.178/laravel/public/img/rotate/5.png",
+        "http://182.254.161.178/laravel/public/img/rotate/6.png",
+        "http://182.254.161.178/laravel/public/img/rotate/7.png",
+        "http://182.254.161.178/laravel/public/img/rotate/8.png",
+        "http://182.254.161.178/laravel/public/img/rotate/9.png",
+        "http://182.254.161.178/laravel/public/img/rotate/10.png",
+        "http://182.254.161.178/laravel/public/img/rotate/11.png",
+        "http://182.254.161.178/laravel/public/img/rotate/12.png",
+        "http://182.254.161.178/laravel/public/img/rotate/13.png",
+        "http://182.254.161.178/laravel/public/img/rotate/14.png",
+        "http://182.254.161.178/laravel/public/img/rotate/15.png",
+        "http://182.254.161.178/laravel/public/img/rotate/16.png",
+        "http://182.254.161.178/laravel/public/img/rotate/17.png",
+        "http://182.254.161.178/laravel/public/img/rotate/18.png",
+        "http://182.254.161.178/laravel/public/img/rotate/19.png",
+        "http://182.254.161.178/laravel/public/img/rotate/20.png",
+        "http://182.254.161.178/laravel/public/img/rotate/21.png",
     ]);
     imgdownload.load();
     // console.log(res);
@@ -100,7 +100,7 @@ $(function(){
                     // console.log(str)
                 //}
             }
-            var src="http://182.254.161.178/test/img/rotate/"+str+".png";
+            var src="http://182.254.161.178/laravel/public/img/rotate/"+str+".png";
             $("#rotate").attr("src",src);
         },70);
         setTimeout(function(){
