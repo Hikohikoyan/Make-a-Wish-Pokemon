0fea6854b51f4cda0c783d0e9374252fd8c0f474
tenseven <243594043@qq.com>
2019-05-18 12:06:23 +0800

    correct show_wishes.php

--- first_laravel/app/Http/Controllers/show_wishes.php
+++ first_laravel/app/Http/Controllers/show_wishes.php
@@ -13,21 +13,26 @@ class show_wishes extends Controller
         $openid=$request->session()->get('openid');
         $all_my_help=DB::table('custom_wish')
         ->where('helper_id',$openid)
+        ->select('id','wish_content')
         ->get();
         return response()->json($all_my_help);
     }
+
     public function my_wishes( Request $request){
         $openid=$request->session()->get('openid');           
         $all_my_wishes=DB::table('custom_wish')
         ->where('wisher_id',$openid)
+        ->select('id','wish_content','situation')
         ->get();
         return response()->json($all_my_wishes);
     }
+
     public function help_wish( Request $request){
         $fetch_wish=DB::table('custom_wish')
                     ->where("situation","未领取")
                     ->inRandomOrder()
                     ->take(3)
+                    ->select('id','wish_content')
                     ->get();
         
         return response()->json($fetch_wish);
