9c22ccc8b77dc77d9d9a6a07ebbc63f02c5dc87c
Hikoyan <huanghongyan324@gmail.com>
2019-05-19 15:09:04 +0800

    pre wishes

--- js/display.js
+++ js/display.js
@@ -9,10 +9,10 @@ $(function () {
     var wishText;//愿望文本公用存储
     var wishes=new Array();//愿望ID公用存储
     var wisher_id=new Array();
-    $('html').css({
-        'height':$(document).height(),
-        'width':$(document).width()
-    })
+    // $('html').css({
+    //     'height':$(document).height(),
+    //     'width':$(document).width()
+    // })
     $("#img41").click(function(){//开头的GO
         $("#loading").remove();
         $("#index").show();
